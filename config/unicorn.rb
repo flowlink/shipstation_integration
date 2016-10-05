378782c8303ef687bf90aeae4ba170c93e610a63
Sarah Anderson <seanderson07@gmail.com>
2016-10-05 11:37:21 -0400

    Removes commented out unicorn configuration.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,3 @@
-# @dir = File.expand_path(File.dirname(__FILE__))
-# listen File.join(@dir, "../unicorn.sock"), :backlog => 1024
-
 worker_processes ENV.fetch('WORKER_PROCESSES', 3).to_i
 timeout ENV.fetch('WORKER_TIMEOUT', 240).to_i
 
